9ab8d5ba879a800ccb0ba41c7210a27627c81862
riw <riw@wip.tu-berlin.de>
2020-01-15 21:28:23 +0100

    conf fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ autosummary_imported_members = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+master_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
