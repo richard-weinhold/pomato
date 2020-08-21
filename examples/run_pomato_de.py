457c804a08c7368b09392b0e37225da247fbb9ae
riw <riw@wip.tu-berlin.de>
2020-08-21 20:14:59 +0200

    Update example to be in line with documentation

--- examples/run_pomato_de.py
+++ examples/run_pomato_de.py
@@ -5,12 +5,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-
-# %% Init POMATO with the options file and the dataset
+# Init POMATO with the options file and the dataset
 from pomato import POMATO
 
-mato = POMATO(wdir=Path.cwd(), options_file="profiles/de.json")
-mato.load_data(r'data_input/dataset_de_new.xlsx')
+wdir = Path(__file__).parent
+mato = POMATO(wdir=wdir, options_file="profiles/de.json")
+mato.load_data('data_input/dataset_de.zip')
 
 # Acess the data.
 nodes = mato.data.nodes
@@ -22,27 +22,44 @@ plants = mato.data.plants
 availability = mato.data.availability
 net_export = mato.data.net_export
 
-# %% Run the Market Model, including (costbased) Re-Dispatch.
+mato.data.visualize_inputdata()
+# Run the Market Model, including (costbased) Re-Dispatch.
 # The Market Result is determined based on the option file.
 # The Redispatrch is done to N-0 per default.
+
+mato.run_market_model()
+market_result = next(iter(mato.data.results.values()))
+
+# Some result analysis
+df1, df2 = market_result.overloaded_lines_n_0()
+gen = pd.merge(mato.data.plants, market_result.G, left_index=True, right_on="p")
+util = gen[["plant_type", "g_max", "G"]].groupby("plant_type").sum()
+print(util.G / util.g_max)
+
+# Show Geo Plot
+mato.create_geo_plot()
+
+# Change options, re-create grid representation and re-run
+mato.data.results = {}
+mato.options["optimization"]["redispatch"]["include"] = True
+mato.options["optimization"]["redispatch"]["zones"] = ["DE"]
+mato.options["optimization"]["redispatch"]["cost"] = 50
+
 mato.create_grid_representation()
-mato.update_market_model_data()
 mato.run_market_model()
 
-# There are two market results loaded into data.results.
-# Specify redisp and market result for analysis
-redisp_result = mato.data.results[next(r for r in list(mato.data.results) if "redispatch" in r)]
-market_result = mato.data.results[next(r for r in list(mato.data.results) if "market_result" in r)]
+market_result, redisp_result = mato.data.return_results()
 
-# Check for Overloaded lines N-0, N-1 (should be non for N-0, but plenty for N-1)
-df1, df2 = redisp_result.overloaded_lines_n_1()
-df3, df4 = redisp_result.overloaded_lines_n_0()
+# Check for Overloaded lines N-0
+n0_m, _ = market_result.overloaded_lines_n_0()
+print("Number of N-0 Overloads: ", len(n0_m))
+
+n0_r, _  = redisp_result.overloaded_lines_n_0()
+print("Number of N-0 Overloads: ", len(n0_r))
 
 # Check for infeasibilities in market / redispatch result.
 infeas_redisp = redisp_result.infeasibility()
 infeas_market = market_result.infeasibility()
-
-# %% Examplary Result Analysis
 
 # Generation comparison between Market Result and Redispatch.
 gen = pd.merge(market_result.data.plants[["plant_type", "fuel", "tech", "g_max", "node"]],
@@ -53,20 +70,12 @@ gen = pd.merge(gen, redisp_result.G, on=["p", "t"], suffixes=("_market", "_redis
 gen["delta"] = gen["G_redispatch"] - gen["G_market"]
 gen["delta_abs"] = gen["delta"].abs()
 
-# Generation Plots
-gen[["fuel", "t", "G_market"]].groupby(["t", "fuel"]).sum().reset_index().pivot(index="t", columns="fuel", values="G_market").plot.area(stacked=True)
-gen[["fuel", "t", "G_redispatch"]].groupby(["t", "fuel"]).sum().reset_index().pivot(index="t", columns="fuel", values="G_redispatch").plot.area(stacked=True)
-# plt.show()
-
 # Redispatch Values
 gen.delta.sum()
 gen.delta_abs.sum()/24
 
-# %% Bokeh PLot
-mato.create_geo_plot(name="DE")
+# Create Geo PLot
+mato.create_geo_plot(title="DE: Redispatch")
+# mato.bokeh_plot.save_plot(mato.wdir.joinpath("bokeh_redispatch.html")
+
 mato._join_julia_instances()
-
-# %% Run Interactive Bokeh Plot
-# mato.bokeh_plot.add_market_result(redisp_result, name="redispatch de")
-# mato.bokeh_plot.start_server()
-
--- examples/run_pomato_ieee.py
+++ examples/run_pomato_ieee.py
@@ -1,14 +1,14 @@
 """IEEE Test Case."""
-# %%
+#
 import sys
 from pathlib import Path
 import numpy as np
 import pandas as pd
 
-# %% Init POMATO with the options file and the dataset
+# Init POMATO with the options file and the dataset
 import pomato
 
-wdir = Path.cwd()
+wdir = Path(__file__).parent
 mato = pomato.POMATO(wdir=wdir, options_file="profiles/ieee118.json")
 mato.load_data('data_input/pglib_opf_case118_ieee.m')
 
@@ -20,18 +20,21 @@ zones = mato.data.zones
 plants = mato.data.plants
 
 # %% Run Uniform Prcing Market Clearing
-
 mato.options["optimization"]["type"] = "dispatch"
 mato.create_grid_representation()
-mato.update_market_model_data()
 mato.run_market_model()
 
 result_folder = mato.market_model.result_folders[0]
 result = mato.data.results[result_folder.name]
 
 # Check Overloaded Lines for N-0 and N-1 contingency cases.
-df1, df2 = result.overloaded_lines_n_1()
-df3, df4 = result.overloaded_lines_n_0()
+df1, df2 = result.overloaded_lines_n_0()
+df3, df4 = result.overloaded_lines_n_1()
+
+print("Number of overloaded lines (Dispatch): ", len(df1))
+print("Number of overloaded lines N-1 (Dispatch): ", len(df3))
+
+# mato.create_geo_plot()
 
 # %% Run N-0 Market Clearing
 mato.options["optimization"]["type"] = "nodal"
@@ -43,13 +46,17 @@ result_folder = mato.market_model.result_folders[0]
 result = mato.data.results[result_folder.name]
 
 # Check Overloaded Lines for N-0 and N-1 contingency cases.
-df1, df2 = result.overloaded_lines_n_1()
-df3, df4 = result.overloaded_lines_n_0()
+df1, df2 = result.overloaded_lines_n_0()
+df3, df4 = result.overloaded_lines_n_1()
+
+print("Number of overloaded lines (Nodal): ", len(df1))
+print("Number of overloaded lines N-1 (Nodal): ", len(df3))
 
 
 # %% Rerun the model as SCOPF
 mato.options["optimization"]["type"] = "cbco_nodal"
 mato.options["grid"]["cbco_option"] = "clarkson_base"
+
 # Requires to presolve the network with the RedundancyRemvoal Algorith
 # if no previous set of essential indices is privided in the option file
 mato.create_grid_representation()
@@ -62,9 +69,12 @@ mato.run_market_model()
 result_folder = mato.market_model.result_folders[0]
 result = mato.data.results[result_folder.name]
 
-df1, df2 = result.overloaded_lines_n_1()
-df3, df4 = result.overloaded_lines_n_0()
+df1, df2 = result.overloaded_lines_n_0()
+df3, df4 = result.overloaded_lines_n_1()
+
+print("Number of overloaded lines (SCOPF): ", len(df1))
+print("Number of overloaded lines N-1 (SCOPF): ", len(df3))
 
 # %% Show a Geo Plot of the market- and the resulting power flows.
-mato.create_geo_plot(name="IEEE")
+mato.create_geo_plot(title="IEEE")
 mato._join_julia_instances()
