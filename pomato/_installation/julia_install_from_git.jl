af10bb061fed4cdfac01d60df28e917fb18783b6
riw <riw@wip.tu-berlin.de>
2021-01-16 13:31:12 +0100

    Fix: Julia Testing for 1.5 Pull MarketModel from construction

--- pomato/_installation/julia_install_from_git.jl
+++ pomato/_installation/julia_install_from_git.jl
@@ -7,7 +7,7 @@ if isdir("pomato")
 	rm("pomato", recursive=true)
 end
 Pkg.activate("pomato")
-Pkg.add(PackageSpec(url="https://github.com/richard-weinhold/MarketModel.git", rev="master")) 
+Pkg.add(PackageSpec(url="https://github.com/richard-weinhold/MarketModel.git", rev="construction")) 
 Pkg.add(PackageSpec(url="https://github.com/richard-weinhold/RedundancyRemoval.git", rev="master")) 
 Pkg.add("JSON")
 Pkg.add("Clp")
