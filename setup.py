45e5574c708a05fddb4c977004d7aa6551617c86
riw <riw@wip.tu-berlin.de>
2021-05-18 14:23:17 +0200

    Make sure correct MarketModel branch is used.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess, sys, os
 from pathlib import Path
 
 def julia_instantiate(install_lib_path):
-    args = ["julia", "_installation/julia_install_from_git.jl"]   
+    args = ["julia", "_installation/julia_install_from_git.jl", "master", "construction"]   
     # raise ImportError("package path %s", package_path)
     package_path = Path(install_lib_path).joinpath("pomato")
     process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE,
@@ -38,7 +38,7 @@ class InstallCommand(install):
         julia_instantiate(self.install_lib)
 
 setup(name='pomato',
-      version='0.3.1',
+      version='0.3.2',
       description='Power Market Tool',
       author='Richard Weinhold',
       author_email='riw@wip.tu-berlin.de',
