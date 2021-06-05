534b8e083d7cdc1e15577ba11a960e3ff06f409f
riw <riw@wip.tu-berlin.de>
2021-06-05 17:39:24 +0200

    Update Examples. New DE Dataset, IEEE jupyter NB.

--- examples/run_pomato_de.py
+++ examples/run_pomato_de.py
@@ -1,34 +1,42 @@
+# %%
 """DE Test Case."""
-import sys
 from pathlib import Path
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import pomato
 
-# Init POMATO with the options file and the dataset
-wdir = Path("/examples/") # Change to local copy of examples folder
-mato = pomato.POMATO(wdir=wdir, options_file="profiles/de.json")
-mato.load_data('data_input/dataset_de.zip')
 
+# %%
+# wdir = Path("/examples/") # Change to local copy of examples folder
+wdir = Path.cwd()
+mato = pomato.POMATO(wdir=wdir, options_file="profiles/de.json")
+mato.load_data('data_input/DE_2020.zip')
+
+
+# %%
 # Create the grid representation, the options are specified in the 
 # json file supplied in the instantiation. 
-# In this case the model runs a uniform pricing with subsequent redispatch.
+# In this case the model runs a zonal pricing with NTC to neighboring countries
+# and subsequent redispatch.
 mato.create_grid_representation()
 mato.run_market_model()
 
+
+# %%
 # Return the results, in this case two: 
 # market result (uniform pricing, no network constraints)
 # and redispatch (to account for N-0 network feasibility)
 market_result, redisp_result = mato.data.return_results()
 
+
+# %%
 # Check for overloaded lines in the market and redispatch results
 n0_m, _ = market_result.overloaded_lines_n_0()
-print("Number of N-0 Overloads: ", len(n0_m))
+print("Number of N-0 Overloads in market result: ", len(n0_m))
 n0_r, _  = redisp_result.overloaded_lines_n_0()
-print("Number of N-0 Overloads: ", len(n0_r))
+print("Number of N-0 Overloads after redispatch: ", len(n0_r))
 
+
+# %%
 # Generation comparison between Market Result and Redispatch.
 # Redispatch is calculated G_redispatch - G_market as delta
 # The absolute delta represents the total redispatched energy
@@ -39,16 +47,30 @@ gen["delta"] = gen["G_redispatch"] - gen["G_market"]
 gen["delta_abs"] = gen["delta"].abs()
 print("Redispatched energy per hour (abs) [MWh] ", gen.delta_abs.sum()/len(gen.t.unique()))
 
+# Many of these functions are implemented as methods of the Result class
+gen_2 = redisp_result.redispatch()
+print("Redispatched energy per hour (abs) [MWh] ", gen_2.delta_abs.sum()/len(gen_2.t.unique()))
+
+
+# %%
 # Create Geo Plot. DISCLAiMER: The reported prices are the dual 
 # in the redispatch result, thus including costs for redispatch.
-mato.visualization.create_geo_plot(redisp_result, show_prices=True, show_plot=False,
+mato.visualization.create_geo_plot(redisp_result, show_prices=False, show_plot=False, show_redispatch=True,
                                    filepath=mato.wdir.joinpath("geoplot_DE.html"))
 
+
+# %%
 # Create visualization of the generation schedule in the market result. 
 mato.visualization.create_generation_plot(market_result, show_plot=False, 
                                           filepath=mato.wdir.joinpath("generation_plot.html"))
-mato.visualization.create_storage_plot(market_result, show_plot=False, 
-                                       filepath=mato.wdir.joinpath("storage_plot.html"))
 
+
+# %%
+# 
 mato._join_julia_instances()
+
+
+# %%
+
+
 
